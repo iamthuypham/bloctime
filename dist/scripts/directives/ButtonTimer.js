a2430aa4e548cad8ed3fcdee27faa812656cd7c5
Thuy Pham <tpham045@hotmail.com>
2016-11-18 22:25:14 +0000

    From mentor feedback, change a service to directive

--- dist/scripts/directives/ButtonTimer.js
+++ dist/scripts/directives/ButtonTimer.js
@@ -1,22 +1,69 @@
 (function() {
-  
-    function ButtonTimer(Timer, $rootScope) {
-      return {
-        templateUrl: '/templates/directives/button-timer.html',
-        restrict: 'E',
-        scope: {},
-        link: function(scope, element, attributes) {
-          scope.timer = Timer;
-          scope.toRed = false;
-          
-          
-          $rootScope.$on('timerStop', function() {
-            scope.toRed = false;
-          });
+
+  function ButtonTimer($rootScope, $interval, $filter) {
+    return {
+      templateUrl: '/templates/directives/button-timer.html',
+      restrict: 'E',
+      scope: {},
+      link: function(scope, element, attributes) {
+        scope.toRed = false;
+        scope.status = true;
+        scope.Tracker = null;
+        scope.currentTime = null;
+        scope.currentTimeString = '25:00';
+        /**
+         * @function start
+         * @desc 1) display reset button 2) display decrement time
+         */
+        var startTimer = function() {
+          scope.running = true;
+          scope.currentTime = 1500 - 1; //in second
+          scope.Tracker = $interval(function() {
+            var str = $filter('timecode')(scope.currentTime--);
+            scope.currentTimeString = str;
+          }, 1000);
+        };
+        /**
+         * @function stopTimer
+         * @desc 1) display start button 2) stop the decrement 3) reset time to default 25 min
+         */
+        var stopTimer = function() {
+          scope.running = false;
+          //Cancel the time-tracker.
+          if (angular.isDefined(scope.Tracker)) {
+            $interval.cancel(scope.Tracker);
+            // $rootScope.$broadcast('timerStop');
+            scope.currentTimeString = '25:00';
+          }
+        };
+        /**
+         * @function start
+         * @desc To be called in dashboard view when start button is clicked
+         */
+        scope.start = function() {
+          if (scope.status === true) {
+            startTimer();
+          }
+          else {
+            stopTimer();
+          }
+        };
+
+        /**
+         * @function start
+         * @desc To be called in dashboard view when reset button is clicked
+         */
+        scope.stop = function() {
+          stopTimer();
         }
-  
+
+        // $rootScope.$on('timerStop', function() {
+        //   scope.toRed = false;
+        // });
       }
+
     }
+  }
 
   angular
     .module('blocTime')
