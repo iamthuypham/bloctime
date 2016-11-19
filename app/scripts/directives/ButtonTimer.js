a5db2be276d27cdebd93630ef088afe5d2ca50c4
Thuy Pham <tpham045@hotmail.com>
2016-11-19 01:15:00 +0000

    Add finishTimer function to setup break/work session after another

--- app/scripts/directives/ButtonTimer.js
+++ app/scripts/directives/ButtonTimer.js
@@ -6,62 +6,82 @@
       restrict: 'E',
       scope: {},
       link: function(scope, element, attributes) {
+        //@var for button color 
         scope.toRed = false;
-        scope.status = true;
-        scope.Tracker = null;
+        //@var for counting time
+        scope.onBreak = false;
+        scope.counter = null;
         scope.currentTime = null;
-        scope.currentTimeString = '25:00';
+        scope.currentTimeString = '00:20';
         /**
          * @function start
          * @desc 1) display reset button 2) display decrement time
          */
-        var startTimer = function() {
+        var startTimer = function(time) {
           scope.running = true;
-          scope.currentTime = 1500 - 1; //in second
-          scope.Tracker = $interval(function() {
-            var str = $filter('timecode')(scope.currentTime--);
+          scope.toRed = true; //change button color to red
+          scope.currentTime = time; //in second
+          
+
+          scope.counter = $interval(function() {
+            if (scope.currentTime === 0) {
+              finishTimer(time);
+            }
+            var str = $filter('timecode')(scope.currentTime--); //timecode filter return a time in string
             scope.currentTimeString = str;
           }, 1000);
+          
+        };
+        /**
+         * @function finishTimer
+         * @desc when user successfully complete a session. 1) stopTimer 2) set onBreak value to true
+         */
+        var finishTimer = function(time) {
+          if (time === 20){
+            scope.onBreak = true;
+            scope.currentTimeString = 10;
+          } else {
+            scope.onBreak = false;
+            scope.currentTimeString = 20;
+          }
+          stopTimer(scope.currentTimeString);
         };
         /**
          * @function stopTimer
          * @desc 1) display start button 2) stop the decrement 3) reset time to default 25 min
          */
-        var stopTimer = function() {
+        var stopTimer = function(time) {
           scope.running = false;
+          scope.currentTime = time;
           //Cancel the time-tracker.
-          if (angular.isDefined(scope.Tracker)) {
-            $interval.cancel(scope.Tracker);
-            // $rootScope.$broadcast('timerStop');
-            scope.currentTimeString = '25:00';
+          if (angular.isDefined(scope.counter)) {
+            $interval.cancel(scope.counter);
           }
+          var str = $filter('timecode')(scope.currentTime);
+          scope.currentTimeString = str;
+          scope.toRed = false;
         };
         /**
          * @function start
          * @desc To be called in dashboard view when start button is clicked
          */
         scope.start = function() {
-          if (scope.status === true) {
-            startTimer();
+          if (!scope.onBreak) {
+            startTimer(20);
           }
-          else {
-            stopTimer();
+          else if (scope.onBreak) {
+            startTimer(scope.currentTime);
           }
         };
-
         /**
          * @function start
          * @desc To be called in dashboard view when reset button is clicked
          */
         scope.stop = function() {
-          stopTimer();
+          stopTimer(20);
         }
-
-        // $rootScope.$on('timerStop', function() {
-        //   scope.toRed = false;
-        // });
+        
       }
-
     }
   }
 
