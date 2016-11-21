ac6147970127cfd030140c8afc4fbb0a878288c7
Thuy Pham <tpham045@hotmail.com>
2016-11-21 01:34:07 +0000

    1) Add new directive task-setting 2)Able to add/remove task

---
+++ dist/scripts/directives/TaskSetting.js
@@ -0,0 +1,38 @@
+(function() {
+
+  function TaskSetting(Tasks) {
+    return {
+      templateUrl: '/templates/directives/task-setting.html',
+      restrict: 'E',
+      scope: {},
+      link: function(scope, element, attributes) {
+        //@var for tasks array
+        var taskArray = Tasks.all;
+        scope.taskList = taskArray;
+        /**
+        * @func updateTask
+        * @desc 1) read user input 2) write to database 3)reset input field to default
+        */
+        scope.updateTask = function(task) {
+          if (scope.task) {
+            scope.task = angular.copy(task);
+            taskArray.$add(scope.task);
+            scope.task = '';
+          };
+        };
+        /**
+        * @func removeTask
+        * @desc 1) remove a task using its index
+        */
+        scope.removeTask = function(index) {
+          Tasks.all.$remove(index);
+        }
+
+      }
+    };
+  }
+
+  angular
+    .module('blocTime')
+    .directive('taskSetting', TaskSetting);
+})();
--- dist/scripts/services/Tasks.js
+++ dist/scripts/services/Tasks.js
@@ -1,10 +1,9 @@
 (function() {
   function Tasks($firebaseArray) {
     var ref = firebase.database().ref();
-
     // download tasks into a synchronized array
     var tasks = $firebaseArray(ref);
-
+    
     return {
       all: tasks
         // remaining logic for tasks
